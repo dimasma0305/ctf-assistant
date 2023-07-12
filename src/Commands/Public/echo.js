f785b6c8a50dafbb0a6c60b2dfee976a25a4ad05
dimasma0305 <dimasmaulana0305@gmail.com>
2023-07-12 09:10:49 +0800

    add echo command

---
+++ src/Commands/Public/echo.js
@@ -0,0 +1,37 @@
+const {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    Client
+} = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("echo")
+        .setDescription("will echo"),
+    /**
+     *
+     * @param {ChatInputCommandInteraction} interaction
+     * @param {Client} _client
+     */
+    async execute(interaction, _client) {
+        const permissionAdmin = [ManageRoles, ManageChannels];
+        if (!interaction.member.permissions.has(permissionAdmin)) {
+            return interaction.reply({
+                content: "This command is only available to the admin",
+                ephemeral: true,
+            });
+        }
+
+        const { options } = interaction;
+        const text = options.getString("text");
+        if (!text) {
+            return interaction.reply({
+                content: "you need to provide the text",
+                ephemeral: true
+            })
+        }
+        interaction.reply(text)
+
+
+    },
+};
