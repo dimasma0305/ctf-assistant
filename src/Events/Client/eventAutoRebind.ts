a470ec009678374a10da6b2f1c2ce9eb2016f8ea
dimas <dimasmaulana0305@gmail.com>
2024-06-11 10:25:34 +0800

    chore: Refactor eventAutoRebind.ts

--- src/Events/Client/eventAutoRebind.ts
+++ src/Events/Client/eventAutoRebind.ts
@@ -1,12 +1,8 @@
 import { Event } from "../../Handlers/eventHandler"
 import { MyClient } from "../../Model/client";
-import { getUpcommingOnlineEvent, infoEvent } from "../../Functions/ctftime-v2";
+import { infoEvent } from "../../Functions/ctftime-v2";
 import { ReactionRoleEvent } from "../../Commands/Public/Ctftime/utils/event";
 import { createRoleIfNotExist } from "../../Commands/Public/Ctftime/utils/event_utility";
-import { getEventsParticipants } from "../../Functions/ctftime";
-import { sleep } from "bun";
-import cron from "node-cron"
-import { Guild } from "discord.js";
 
 const EVENT_ID_REGEX = /\/event\/(\d+)\//;
 
@@ -34,41 +30,6 @@ export const event: Event = {
                 })
                 await reactionRoleEvent.addEvent()
             });
-            if (guild.name == "TCP1P Server") {
-                await runEvents(guild)
-                cron.schedule("0 0 * * *", async () => await runEvents(guild));
-            }
         })
     },
-}
-
-async function runEvents(guild: Guild) {
-    const upcomming = await getUpcommingOnlineEvent(7);
-    for (const id in upcomming) {
-        const scheduledEvents = await guild.scheduledEvents.fetch();
-        const ctfEvent = upcomming[id];
-        const event = scheduledEvents.find((ev) => {
-            const match = ev.url.match(EVENT_ID_REGEX);
-            if (!match) return;
-            const id = match[1];
-            return id == ctfEvent.id.toString();
-        });
-        if (event) return;
-        const participants = await getEventsParticipants(ctfEvent.id.toString());
-        await sleep(1000);
-        for (const id in participants) {
-            if (participants[id] == "TCP1P") {
-                const reactionRoleEvent = new ReactionRoleEvent(guild, {
-                    ctfEvent: ctfEvent,
-                    notificationRole: await createRoleIfNotExist({
-                        name: "CTF Waiting Role",
-                        guild: guild,
-                        color: "#87CEEB"
-                    })
-                });
-                await reactionRoleEvent.addEvent();
-                break;
-            }
-        }
-    }
 }
