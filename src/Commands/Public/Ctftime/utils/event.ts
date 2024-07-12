8d5f845696ebc2075ae64d237cf5fb2e2ec25b13
dimas <dimasmaulana0305@gmail.com>
2024-07-12 20:43:25 +0800

    Refactor event.ts to limit the description length in the absence of an event

--- src/Commands/Public/Ctftime/utils/event.ts
+++ src/Commands/Public/Ctftime/utils/event.ts
@@ -57,7 +57,7 @@ Selamat datang di channel ini, tempatnya untuk berbagi writeup seru dari CTF ${c
         var event = this.guild.scheduledEvents.cache.find((event)=>event.name===this.options.ctfEvent.title && !event.isCanceled() && !event.isCompleted())
 
         if (!event){
-            const description = this.options.ctfEvent.description
+            const description = this.options.ctfEvent.description.length > 1000 ? this.options.ctfEvent.description.slice(0, 1000) : this.options.ctfEvent.description
             const ctftime_url = this.options.ctfEvent.ctftime_url
             const url = this.options.ctfEvent.url
             const organizers = this.options.ctfEvent.organizers
